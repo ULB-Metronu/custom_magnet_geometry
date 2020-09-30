3fb49f7f5bbe6ded1304262090285bc23a1153d7
Eliott Ramoisiaux <Eliott.ramoisiaux@ulb.be>
2020-09-30 14:11:20 +0200

    first commit

---
+++ source/UDipoleConstructor.cc
@@ -0,0 +1,46 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2020.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "UDipole.hh"
+#include "UDipoleConstructor.hh"
+
+#include "BDSAcceleratorComponent.hh"
+#include "parser/element.h" // for GMAD::Element
+
+#include "CLHEP/Units/SystemOfUnits.h"
+
+#include "G4Types.hh" // for G4String etc
+
+BDSAcceleratorComponent* UDipoleConstructor::Construct(GMAD::Element const* element,
+						       GMAD::Element const* /*prevElement*/,
+						       GMAD::Element const* /*nextElement*/,
+						       G4double /*currentArcLength*/,
+						       G4double /*brhoIn*/,
+						       G4double /*beta0In*/)
+{
+  G4String params = G4String(element->userParameters);
+  
+  // Here we pull out the information we want from the parser element and construct
+  // one of our components and return it. BDSIM will delete this at the end.
+  // If you don't need to use all the parameters, use an /*inline*/ comment to
+  // avoid a compiler warning about an unused variable.
+  BDSAcceleratorComponent* dipole = new UDipole(element->name,
+						element->B*CLHEP::tesla,
+						params);
+  return dipole;
+}
---
+++ usercomponent.cc
@@ -0,0 +1,43 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2020.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "BDSIMClass.hh" // bdsim interface
+
+#include "UDipole.hh"
+#include "UDipoleConstructor.hh"
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+  // construct an instance of bdsim
+  BDSIM* bds = new BDSIM();
+
+  // register a custom component by name udipole with a user-provided constructor
+  // BDSIM will delete the constructor at the end.
+  bds->RegisterUserComponent("udipole", new UDipoleConstructor());
+
+  // construct geometry and physics
+  bds->Initialise(argc, argv);
+  if (!bds->Initialised()) // check if there was a problem.
+    {std::cout << "Intialisation failed" << std::endl; return 1;}
+  
+  bds->BeamOn(); // run the simulation
+  delete bds;    // clean up
+  return 0;      // exit nicely
+}
